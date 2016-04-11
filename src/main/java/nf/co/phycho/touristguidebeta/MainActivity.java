772448c0937e8875e65a1502fe713c083feb88f9
RonakSuchak <suchakronak16@gmail.com>
2016-04-11 11:08:00 +0530

    Initial commit

---
+++ src/main/java/nf/co/phycho/touristguidebeta/MainActivity.java
@@ -0,0 +1,52 @@
+package nf.co.phycho.touristguidebeta;
+
+import android.content.Intent;
+import android.os.HandlerThread;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressBar mProgressbar;
+    Thread thread;
+    Runnable r;
+    String logTag = "nf.co.phycho.touristguidebeta";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mProgressbar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressbar.setVisibility(View.VISIBLE);
+        try{newActivity();}catch (Exception e){e.printStackTrace();}
+
+    }
+
+    public void newActivity(){
+        Log.v("at newActivity",logTag);
+        android.os.Handler handler = new android.os.Handler();
+        Log.v("handler created", logTag);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.v("at run", logTag);
+                    Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                    Log.v("intent created", logTag);
+                    startActivity(i);
+                    Log.v("activity started", logTag);
+
+                }
+            }, 3000);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+}
+
